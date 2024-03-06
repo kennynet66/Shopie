9721af027c4c6ecede3b8001a97a90441ce03d30
Kenedy Maina <kennynet66@gmail.com>
2024-03-06 15:13:26 +0300

    implemented guards

--- shopieNg/src/app/Guards/auth.guard.ts
+++ shopieNg/src/app/Guards/auth.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { DataService } from '../services/data.service';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
